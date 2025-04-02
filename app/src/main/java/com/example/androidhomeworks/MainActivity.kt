893582f4c9fb9e020d91caef7477b6fbc45e2eea
Aliaksandra Varabyova <alexvor92@gmail.com>
2025-04-02 17:51:40 +0300

    added lesson18 homework

--- app/src/main/java/com/example/androidhomeworks/MainActivity.kt
+++ app/src/main/java/com/example/androidhomeworks/MainActivity.kt
@@ -1,15 +1,58 @@
 package com.example.androidhomeworks
 
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.widget.Switch
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.widget.SwitchCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var preferences: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+
+        preferences = getSharedPreferences("theme_prefs", MODE_PRIVATE)
+
+        val isDarkMode = preferences.getBoolean("isDarkMode", false)
+        if (isDarkMode) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        }
+
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<android.widget.Button>(R.id.button)
+        val themeSwitch = findViewById<SwitchCompat>(R.id.themeSwitch)
+        themeSwitch.isChecked = isDarkMode
+
+        themeSwitch.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                savePreferences(true)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                savePreferences(false)
+            }
+
+        }
+
+        button.setOnClickListener() {
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun savePreferences(isDarkMode: Boolean) {
+        preferences.edit().putBoolean("isDarkMode", isDarkMode).apply()
     }
 }
