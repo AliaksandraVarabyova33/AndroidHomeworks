09a152287e56ff0af9517a6dc1840a18fe8d1d2f
Aliaksandra Varabyova <alexvor92@gmail.com>
2025-04-07 13:23:50 +0300

    added lesson 19 homework

--- app/src/main/java/com/example/androidhomeworks/SecondActivity.kt
+++ app/src/main/java/com/example/androidhomeworks/SecondActivity.kt
@@ -1,15 +1,28 @@
 package com.example.androidhomeworks
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.androidhomeworks.MainActivity.Companion.ACTIVITY_STRING_KEY
 
 class SecondActivity : AppCompatActivity() {
+
+    private lateinit var textView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContentView(R.layout.activity_second)
+        textView = findViewById(R.id.textView)
+        textView.text = intent.getStringExtra(ACTIVITY_STRING_KEY)
+
+        val text = getString(R.string.my_fragment)
+        showMyFragment(text)
+    }
+
+    private fun showMyFragment(text: String) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.myFragmentContainer, MyFragment.getInstance(text))
+            .commit()
     }
 }
